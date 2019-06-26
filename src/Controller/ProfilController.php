227f4d10897dbe7ffdf3416af94e98bd1e29f662
legallclaire <legall_claire@hotmail.fr>
2019-06-26 13:24:21 +0200

    [update] ProfilController.php [update] ParticipantsType.php

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Participants;
+use App\Form\ParticipantsType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends Controller
 {
     /**
-     * @Route("/gererProfil", name="profil_gerer")
+     * @Route("/gererProfil/{id}", name="profil_gerer", requirements={"id"="\d+"})
      */
-    public function gererProfil()
+    public function gererProfil($id,EntityManagerInterface $em, Request $request)
     {
-        return $this->render('profil/gererProfil.html.twig');
+        $participant = $em->getRepository(Participants::class)->find($id);
+
+        if ($participant ==null) {
+
+            throw $this->createNotFoundException("Participant inconnu");
+        }
+
+        $participant->setPseudo($participant->getPseudo());
+        $participant->setNom($participant->getNom());
+        $participant->setPrenom($participant->getPrenom());
+
+        if ($participant->getTelephone() !==null) {
+            $participant->setTelephone($participant->getTelephone());}
+
+
+        $participant->setMail($participant->getMail());
+        $participant->setSitesNoSite($participant->getMail());
+
+
+        $participantForm = $this->createForm(ParticipantsType::class,$participant);
+
+        $participantForm->handleRequest($request);
+        if ($participantForm->isSubmitted() && $participantForm->isValid()) {
+
+
+            $em->persist($participant);
+            $em-> flush();
+
+            $this->addFlash("success", "Votre profil a bien été modifié");
+            return $this->redirectToRoute("profil/afficherProfil.html.twig", ['id' => $participant->getId()]);
+
+        }
+
+
+        return $this->render('profil/gererProfil.html.twig', [
+            "participantForm" => $participantForm->createView()
+        ]);
     }
 
     /**
--- src/Form/ParticipantsType.php
+++ src/Form/ParticipantsType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Participants;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,7 +20,8 @@ class ParticipantsType extends AbstractType
             ->add('prenom', TextType::class, ['label'=>'Prénom','attr'=>['class'=>'prenom-class']])
             ->add('telephone', TextType::class, ['label'=>'Téléphone','attr'=>['class'=>'tel-class'], 'required'=> false])
             ->add('mail', TextType::class, ['label'=>'Email','attr'=>['class'=>'mail-class']])
-            ->add('mot_de_passe')
+            ->add('mot_de_passe', RepeatedType::class,['type'=>PasswordType::class, 'invalid_message'=>'Vous n\'avez pas saisi le même mot de passe','first_options'=>
+                ['label'=> 'Mot de passe'], 'second_options'=>['label' => 'Confirmation du mot de passe']])
             ->add('sites_no_site', EntityType::class, ['class'=>'App\Entity\Sites','choice_label'=>'nom_site','placeholder'=>'Choisir une ville'])
             ->add('urlPhoto', TextType::class, ['label'=>'Ma Photo','attr'=>['class'=>'photo-class'],'required'=> false])
 
