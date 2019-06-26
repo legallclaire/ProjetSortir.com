d319300491ed1f53eb00b24e3bd266da1a137d93
Karl <karl.faure2019@campus-eni.fr>
2019-06-26 10:32:08 +0200

    adds : add SortiesController & twig

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Sorties;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortiesController extends Controller
@@ -10,10 +13,32 @@ class SortiesController extends Controller
     /**
      * @Route("/home", name="sorties_home")
      */
-    public function index()
+    public function home()
     {
         return $this->render('sorties/home.html.twig', [
             'controller_name' => 'SortiesController',
         ]);
     }
+
+
+    /**
+     * @Route("/afficherSorties", name="sorties_afficher")
+     */
+    public function afficherSorties()
+    {
+        return $this->render('sorties/afficherSorties.html.twig', [
+            'controller_name' => 'SortiesController',
+        ]);
+    }
+
+    /**
+     * @Route("/gererSorties", name="sorties_gerer")
+     */
+    public function gererSorties()
+    {
+        return $this->render('sorties/gererSorties.html.twig', [
+            'controller_name' => 'SortiesController',
+        ]);
+    }
+
 }
