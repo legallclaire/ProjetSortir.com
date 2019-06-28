c555e8c7f1fae39f67be6bf647d26539be7300e3
legallclaire <legall_claire@hotmail.fr>
2019-06-28 15:14:10 +0200

    [update] script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,55 +1,66 @@
 //script de modification et de suppression des sites :
 
-var elementLigneSite = document.getElementsByClassName('ligneSite');
+    function modificationSite(elementModificationSite) {
 
+        var elementInputNomSite = elementModificationSite.parentNode.parentNode.querySelector('.inputNomSite');
 
-for (var i=0; i<elementLigneSite.length; i++) {
+        elementInputNomSite.disabled = false;
 
-        //var elementModificationSite = elementLigneSite[i].getElementsByClassName('modificationSite')[0];
-       // var elementInputNomSite = elementLigneSite[i].getElementsByClassName("inputNomSite")[0];
+            elementInputNomSite.addEventListener('focus', function() {
 
-    var elementInputNomSite = elementLigneSite[i].querySelector('.inputNomSite');
-    var elementModificationSite = elementLigneSite[i].querySelector('.modificationSite');
+                elementModificationSite.innerHTML = "<input type='submit' value='Enregistrer'>"
 
-        elementModificationSite.onclick = function modificationSite() {
+        });
 
+        elementInputNomSite.addEventListener('blur', function() {
 
-            elementInputNomSite.disabled = false;
+            elementInputNomSite.disabled = true;
+            elementModificationSite.innerHTML = "Modifier"
 
-            elementInputNomSite.onclick = function () {
-
-                elementModificationSite.innerHTML = "<input type='submit' value='Enregistrer'>";
-
-            }
-
-        }
-}
-// script de modification et suppression des villes :
-
-var elementLigneVille = document.getElementsByClassName('ligneVille');
-
-for (var j=0; j<elementLigneVille.length; j++) {
-
-    var elementModificationVille =elementLigneVille[j].querySelector('.modificationVille');
-    var elementInputNomVille = elementLigneVille[j].querySelector('.inputNomVille');
-    var elementInputCodePostalVille = elementLigneVille[j].querySelector('.inputCodePostalVille');
-
-
-    elementModificationVille.onclick = function modificationVille() {
-
-        elementInputNomVille.disabled = false;
-        elementInputCodePostalVille.disabled = false;
-
-
-        function afficherBouttonEnregistrer() {
-
-            elementModificationVille.innerHTML = "<input type='submit' value='Enregistrer'>";
-
-        }
-
-        elementInputNomVille.onclick = afficherBouttonEnregistrer();
-
-        elementInputCodePostalVille.onclick = afficherBouttonEnregistrer();
+        });
 
     }
+
+// script de modification et suppression des villes :
+
+function modificationVille(elementModificationVille) {
+
+    var elementInputNomVille = elementModificationVille.parentNode.parentNode.querySelector('.inputNomVille');
+    var elementInputCodePostalVille = elementModificationVille.parentNode.parentNode.querySelector('.inputCodePostalVille');
+
+
+    elementInputNomVille.disabled = false;
+    elementInputCodePostalVille.disabled = false;
+
+
+    elementInputNomVille.addEventListener('focus', function () {
+
+        elementModificationVille.innerHTML = "<input type='submit' value='Enregistrer'>"
+
+    });
+
+    elementInputNomVille.addEventListener('blur', function () {
+
+        elementInputNomVille.disabled = true;
+        elementInputCodePostalVille.disabled = true;
+        elementModificationVille.innerHTML = "Modifier"
+
+    });
+
+    elementInputCodePostalVille.addEventListener('focus', function () {
+
+        elementModificationVille.innerHTML = "<input type='submit' value='Enregistrer'>"
+
+    });
+
+    elementInputCodePostalVille.addEventListener('blur', function () {
+
+        elementInputNomVille.disabled = true;
+        elementInputCodePostalVille.disabled = true;
+        elementModificationVille.innerHTML = "Modifier"
+
+    });
+
 }
+
+
