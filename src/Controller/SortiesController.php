fdbd755395832fc8b9a324f0e6cc9a9a0e7281ca
Aimeric <aimericsylvain@yahoo.fr>
2019-07-02 16:01:09 +0200

    refacto : changes path link

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -67,7 +67,7 @@ class SortiesController extends Controller
     }
 
     /**
-     * @Route("/", name="sorties_rechercher")
+     * @Route("/rechercherSorties", name="sorties_rechercher")
      */
     public function rechercherSorties(Request $request)
     {
@@ -76,7 +76,7 @@ class SortiesController extends Controller
         $mot = $request->request->get('mot');
         $sortieRepo = $this->getDoctrine()->getRepository(Sorties::class);
         $sortiesRecherchees = $sortieRepo->findSortieRecherche($mot);
-        return $this->redirectToRoute('sorties/afficherSorties.html.twig', [
+        return $this->render('sorties/afficherSorties.html.twig', [
             'listeRecherche' => $sortiesRecherchees,
             'listeSites' => $listeSites,
         ]);
