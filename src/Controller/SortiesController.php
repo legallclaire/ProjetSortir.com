cb365c9dbc21d45ecac7469e1dedd43e2b7e4af9
legallclaire <legall_claire@hotmail.fr>
2019-07-03 13:41:42 +0200

    [update]

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -82,9 +82,9 @@ class SortiesController extends Controller
             $sortie->setEtat($etatCree);
 
             $dateDuJour = new \DateTime('now');
-            $dateDebutSortie = $sortie.getDatedebut();
-            $dateCloture = $sortie.getDateclosure();
-            $dateFinSortie = $sortie.getDatefin();
+            $dateDebutSortie = $sortie->getDatedebut();
+            $dateCloture = $sortie->getDateclosure();
+            $dateFinSortie = $sortie->getDatefin();
 
             if ($dateCloture>$dateDuJour) {
 
@@ -106,18 +106,14 @@ class SortiesController extends Controller
                 $sortie->setEtat($etatPasse);
             }
 
-
-
             //fin de gestion des états
-
-
 
             $em->persist($sortie);
             $em->flush();
             $this->addFlash("success", "Sortie créée !");
             return $this->redirectToRoute("sorties_home", [
                 "id" => $sortie->getId(),
-                "listeVilles => $listeVilles"]);
+                "listeVilles" => $listeVilles]);
         }
 
         return $this->render('sorties/gererSorties.html.twig', [
@@ -144,7 +140,6 @@ class SortiesController extends Controller
 
             $lieuxRepo = $this->getDoctrine()->getRepository(Lieux::class);
             $listeLieux = $lieuxRepo->findByVille($id);
-
 
             $json_data = array();
             $boucle = 0;
