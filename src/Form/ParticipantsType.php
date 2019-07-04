d5ca2eac5a765a0c86f6a4808104f9f497b41c75
Karl <karl.faure2019@campus-eni.fr>
2019-07-04 14:55:34 +0200

    add : adds

--- src/Form/ParticipantsType.php
+++ src/Form/ParticipantsType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\Participants;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ParticipantsType extends AbstractType
 {
@@ -24,7 +26,23 @@ class ParticipantsType extends AbstractType
             ->add('mot_de_passe', RepeatedType::class,['type'=>PasswordType::class, 'invalid_message'=>'Vous n\'avez pas saisi le même mot de passe','first_options'=>
                 ['label'=> 'Mot de passe', 'attr'=>['placeholder'=>'Saisir votre mot de passe']], 'second_options'=>['label' => 'Confirmation du mot de passe','attr'=>['placeholder'=>'Saisir votre mot de passe']]])
             ->add('site', EntityType::class, ['label' => 'Ville de rattachement', 'class'=>'App\Entity\Sites','choice_label'=>'nom_site','placeholder'=>'Choisir une ville'])
-            ->add('urlPhoto', TextType::class, ['label'=>'Ma Photo','attr'=>['class'=>'photo-class'],'required'=> false])
+            ->add('urlPhoto',
+                FileType::class,
+                [
+                    'label'=>'Ma Photo',
+                    'required'=> false,
+                    'mapped'=>false,
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '5M',
+                            'mimeTypes' => [
+                                'image/*'
+                            ],
+                            'mimeTypesMessage' => 'format invalide',
+                        ])
+                    ]
+                ]
+            )
 
         ;
     }
